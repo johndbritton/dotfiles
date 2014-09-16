4a2fa007989362d1d719161126dfaa1e56b2041a
John Britton <public@johndbritton.com>
2014-09-15 17:34:53 -0700

    update screenshots storage location

--- maid/rules.rb
+++ maid/rules.rb
@@ -6,7 +6,7 @@ Maid.rules do
   rule 'Archive screenshots' do
     dir('~/Desktop/Screen Shot [0-9][0-9][0-9][0-9]-*').each do |path|
       if 10.minutes.since?(created_at(path))
-        move(path, '~/Pictures/screenshots')
+        move(path, '/data/media/screenshots')
       end
     end
   end
