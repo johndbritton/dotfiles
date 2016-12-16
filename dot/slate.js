d3d73c26510d55917d684be08a2a2106dc42d70e
John Britton <public@johndbritton.com>
2016-12-16 16:44:53 +0800

    update modifier keys

--- dot/slate.js
+++ dot/slate.js
@@ -1,13 +1,13 @@
 // Needed to do this in the JS config since there's a space in the filename
 // See: https://github.com/jigish/slate/pull/253
-slate.bind("b:cmd;ctrl;shift", function() {
+slate.bind("b:cmd;ctrl;alt", function() {
   slate.shell("/usr/bin/open -a '/Applications/Google Chrome.app'", false, '~/')
 });
 
-slate.bind("c:cmd;ctrl;shift", function() {
+slate.bind("c:cmd;ctrl;alt", function() {
   slate.shell("/usr/bin/open -a '/Applications/Fantastical 2.app'", false, '~/')
 });
 
-slate.bind("d:cmd;ctrl;shift", function() {
+slate.bind("d:cmd;ctrl;alt", function() {
   slate.shell("/usr/bin/open 'Dropbox (JDB)'", false, '~/')
 });
