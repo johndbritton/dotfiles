15f15e3719f79a010345738d19c8836331cc989c
John Britton <public@johndbritton.com>
2020-11-15 20:23:04 -0500

    Fantastical app name changed

--- dot/slate.js
+++ dot/slate.js
@@ -4,10 +4,6 @@ slate.bind("b:cmd;ctrl;alt", function() {
   slate.shell("/usr/bin/open -a '/Applications/Google Chrome.app'", false, '~/')
 });
 
-slate.bind("c:cmd;ctrl;alt", function() {
-  slate.shell("/usr/bin/open -a '/Applications/Fantastical 2.app'", false, '~/')
-});
-
 slate.bind("d:cmd;ctrl;alt", function() {
   slate.shell("/usr/bin/open 'Dropbox (JDB)'", false, '~/')
 });
