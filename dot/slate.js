468c6fa7a605139ffe85cb545d310a61e1cc86ff
John Britton <public@johndbritton.com>
2020-11-16 21:44:25 -0500

    set global application hotkey `b`rowser to Safari.app

--- dot/slate.js
+++ dot/slate.js
@@ -1,9 +1,5 @@
 // Needed to do this in the JS config since there's a space in the filename
 // See: https://github.com/jigish/slate/pull/253
-slate.bind("b:cmd;ctrl;alt", function() {
-  slate.shell("/usr/bin/open -a '/Applications/Google Chrome.app'", false, '~/')
-});
-
 slate.bind("d:cmd;ctrl;alt", function() {
   slate.shell("/usr/bin/open 'Dropbox (JDB)'", false, '~/')
 });
