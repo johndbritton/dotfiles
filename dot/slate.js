14f22ac0ecbfc0f4151fce7a7cd8e0dca63b7357
John Britton <public@johndbritton.com>
2020-11-16 21:44:46 -0500

    remove unnecessary slate.js config file

--- dot/slate.js
+++
@@ -1,5 +0,0 @@
-// Needed to do this in the JS config since there's a space in the filename
-// See: https://github.com/jigish/slate/pull/253
-slate.bind("d:cmd;ctrl;alt", function() {
-  slate.shell("/usr/bin/open 'Dropbox (JDB)'", false, '~/')
-});
