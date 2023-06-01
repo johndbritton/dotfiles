9dc5ea896e2cb2aca25a963f05d6183c9d482800
John Britton <public@johndbritton.com>
2023-06-01 10:57:11 -0400

    Require holding `cmd+q` to quit running application

--- dot/hammerspoon/init.lua
+++ dot/hammerspoon/init.lua
@@ -216,3 +216,31 @@ end)
 hs.hotkey.bind(hyper, "/", function()
   wm.moveWindowToPosition(wm.screenPositions.middle)
 end)
+
+-- Hold cmd + q to quit
+cmdQDelay = 2
+
+cmdQTimer = nil
+cmdQAlert = nil
+
+function cmdQCleanup()
+  hs.alert.closeSpecific(cmdQAlert)
+  cmdQTimer = nil
+  cmdQAlert = nil
+end
+
+function stopCmdQ()
+  if cmdQTimer then
+    cmdQTimer:stop()
+    cmdQCleanup()
+    hs.alert("quit canceled",0.5)
+  end
+end
+
+function startCmdQ()
+  local app = hs.application.frontmostApplication()
+  cmdQTimer = hs.timer.doAfter(cmdQDelay, function() app:kill(); cmdQCleanup() end)
+  cmdQAlert = hs.alert("hold to quit " .. app:name(), true)
+end
+
+cmdQ = hs.hotkey.bind({"cmd"},"q",startCmdQ,stopCmdQ)
